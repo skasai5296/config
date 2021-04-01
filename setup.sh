f6f5b6b85fcd1b7dd3a6c5997cb4a45c8e2a8295
skasai5296 <seito5296@gmail.com>
2021-04-01 18:13:54 +0900

    refactor

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 
-
-install_dotfiles() {
+download_dotfiles() {
   printf "Downloading dotfiles...\n"
   git clone "$DOTFILES_REPO" "$CONFIG_ROOT"
+}
+
+deploy_dotfiles() {
+  # Create symbolic links of dotfiles to home directory.
+
   cd "$CONFIG_ROOT"
-  
-  # Create symbolic links to home.
-  for file in $(find dotfiles/ -maxdepth 1 -name '.[!.]*' -not -name '.git*'); do
-    echo $file
-    echo "$HOME/$(basename $file)"
-    #ln -sfnv "$file" "$HOME/(basename $file)"
+  for file in $(find dotfiles -maxdepth 1 -name '.[!.]*' -not -name '.git*'); do
+    ln -sfnv "$file" "$HOME/$(basename $file)"
   done
 }
 
@@ -21,7 +21,8 @@ fi
 DOTFILES_REPO="git@github.com:skasai5296/dotfiles.git"; export DOTFILES_REPO
 
 main() {
-  install_dotfiles
+  download_dotfiles
+  deploy_dotfiles
 }
 
 main
