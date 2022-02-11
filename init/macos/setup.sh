4f76f129ab3173121653c28e41f1dc26464bc7e7
Seito Kasai <seito.kasai@flywheel.jp>
2022-02-11 22:51:59 +0900

    fix deprecated commands

--- init/macos/setup.sh
+++ init/macos/setup.sh
@@ -17,16 +17,14 @@ if ! exists "brew"; then
   curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash
 fi
 
-brew update && brew upgrade
-
 # Install Google Chrome and Slack unless they're already installed.
 # These can't be done in the Brewfile because the autoupdater
 # breaks Homebrew.
 if [ ! -d "/Applications/Google Chrome.app" ]; then
-  brew cask install google-chrome
+  brew install --cask google-chrome
 fi
 if [ ! -d "/Applications/Slack.app" ]; then
-  brew cask install slack
+  brew install --cask slack
 fi
 
 brew bundle --verbose --force --file="$CONFIG_ROOT/init/macos/Brewfile"
